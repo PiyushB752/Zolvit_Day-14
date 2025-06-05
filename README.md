142a99d1607190e07f3a4311077eef85eede2ffa
Piyush Bachani <Piyush>
2025-06-05 23:48:50 +0530

    Done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Zolvit_Day-14
+
+**Description** -  This repo is about ramping up and down the number of users on GET request of Fake Store API("https://fakestoreapi.com").
---
+++ test/load_test.js
@@ -0,0 +1,26 @@
+import { check } from "k6"
+import http from "k6/http"
+
+export const options = {
+    stages:[
+        {duration:"1s",target:5},
+        {duration:"5s",target:15},
+        {duration:"5s",target:90},
+        {duration:"1s",target:2}
+    ]
+}
+
+export default function(){
+    const res = http.get("https://fakestoreapi.com/products")
+    check(res,{
+        "Status is 200":(e)=>e.status===200,
+        "return array":(e)=>{
+            try{
+                const d = JSON.parse(e.body)
+                return Array.isArray(d) && d.length > 0
+            } catch(err){
+                return false
+            }
+        }
+    })
+}
